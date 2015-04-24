ec90045f81de6dea46d937d9225969ae440537e8
Pawel Chojnacki <pawel@chojnacki.ws>
2015-04-25 01:45:25 +0200

    reduction

--- part2_reduction.cpp
+++ part2_reduction.cpp
@@ -20,13 +20,7 @@ int main(int argc, char* argv[])
 
     omp_set_num_threads(cpu_count);
 
-    volatile double suml[cpu_count*1000];
-//    volatile double *suml = (volatile double*)malloc(100000);
-
-//    volatile double *suml;
-//    posix_memalign((void**)&suml, 64, 4*64);
-
-#pragma omp parallel shared(suml)
+#pragma omp parallel default(none) shared(sum, num_steps, step)
     {
         cpu_set_t cpu;
         CPU_ZERO(&cpu);
@@ -36,19 +30,15 @@ int main(int argc, char* argv[])
 
         printf("%d \n", sched_getcpu());
         double x;
-//        int nCpu =  omp_get_thread_num();
-        int nCpu =  omp_get_thread_num() * (64/sizeof(double));
-        suml[nCpu] = 0.0;
-#pragma omp for
+#pragma omp for reduction(+:sum)
         for (i=0; i<num_steps; i++)
         {
             x = (i + .5)*step;
-            suml[nCpu] = suml[nCpu] + 4.0/(1.+ x*x);
+
+//            #pragma omp atomic
+            sum = sum + 4.0/(1.+ x*x);
         }
 
-
-#pragma omp atomic
-        sum +=  suml[nCpu];
 
     }
 //    sum = suml[0] + suml[1];
